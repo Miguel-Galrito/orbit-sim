ac8d528d60da01ff13014640f9010815ffba45a1
Miguel-Galrito <mapgalrito@gmail.com>
2026-08-28 23:53:35 +0100

    Expose N-body tools in public API

--- src/orbitsim/__init__.py
+++ src/orbitsim/__init__.py
@@ -5,6 +5,14 @@ from .elements import elements_to_state, state_to_elements
 from .propagation import PropagationResult, propagate, propagate_with_maneuvers
 from .maneuvers import bielliptic_transfer, hohmann_transfer, plane_change_delta_v
 from .lambert import lambert_universal
+from .nbody import Body, NBodyResult, propagate_nbody, sun_earth_moon_demo
 from .analysis import altitude_km, ground_track, orbital_period_from_a, specific_energy
 
-__all__ = ["MU_EARTH_KM3_S2","R_EARTH_KM","Maneuver","OrbitalElements","StateVector","PropagationResult","elements_to_state","state_to_elements","propagate","propagate_with_maneuvers","hohmann_transfer","bielliptic_transfer","plane_change_delta_v","lambert_universal","altitude_km","ground_track","orbital_period_from_a","specific_energy"]
+__all__ = [
+    "MU_EARTH_KM3_S2", "R_EARTH_KM", "Maneuver", "OrbitalElements", "StateVector",
+    "PropagationResult", "elements_to_state", "state_to_elements", "propagate",
+    "propagate_with_maneuvers", "hohmann_transfer", "bielliptic_transfer",
+    "plane_change_delta_v", "lambert_universal", "Body", "NBodyResult",
+    "propagate_nbody", "sun_earth_moon_demo", "altitude_km", "ground_track",
+    "orbital_period_from_a", "specific_energy",
+]
