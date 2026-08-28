740d21a152754ff8d086b75b74e6f54dcb7cee3f
Miguel-Galrito <mapgalrito@gmail.com>
2026-08-28 14:34:46 +0100

    Expose expanded orbital design API

--- src/orbitsim/__init__.py
+++ src/orbitsim/__init__.py
@@ -1,7 +1,10 @@
-"""Educational orbital mechanics toolkit."""
-from .constants import MU_EARTH_KM3_S2,R_EARTH_KM
-from .elements import elements_to_state,state_to_elements
-from .maneuvers import HohmannTransfer,hohmann_transfer
-from .propagation import PropagationResult,propagate,propagate_with_maneuvers
-from .types import Maneuver,OrbitalElements,StateVector
-__all__=["MU_EARTH_KM3_S2","R_EARTH_KM","StateVector","OrbitalElements","Maneuver","elements_to_state","state_to_elements","propagate","propagate_with_maneuvers","PropagationResult","HohmannTransfer","hohmann_transfer"]
+"""Orbit Sim public API."""
+from .constants import MU_EARTH_KM3_S2, R_EARTH_KM
+from .types import Maneuver, OrbitalElements, StateVector
+from .elements import elements_to_state, state_to_elements
+from .propagation import PropagationResult, propagate, propagate_with_maneuvers
+from .maneuvers import bielliptic_transfer, hohmann_transfer, plane_change_delta_v
+from .lambert import lambert_universal
+from .analysis import altitude_km, ground_track, orbital_period_from_a, specific_energy
+
+__all__ = ["MU_EARTH_KM3_S2","R_EARTH_KM","Maneuver","OrbitalElements","StateVector","PropagationResult","elements_to_state","state_to_elements","propagate","propagate_with_maneuvers","hohmann_transfer","bielliptic_transfer","plane_change_delta_v","lambert_universal","altitude_km","ground_track","orbital_period_from_a","specific_energy"]
